1962d2820c4196b96986f8e8eb9988130025e4c6
rbs <ricardo.schalch@gmail.com>
2016-09-21 10:21:49 -0300

    Added GOOGLE_API_KEY to settings and created a form for add new business

--- my_community/core/views.py
+++ my_community/core/views.py
@@ -1,7 +1,25 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView
 from django.views.generic import TemplateView
+
+from core.forms import BusinessForm
+from core.models import Business
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+
+class AddBusinessView(CreateView):
+    model = Business
+    fields = ['name', 'address', 'email', 'facebook_page', 'twitter_profile', 'linkedin_profile', 'business_type',
+              'segment']
+    template_name = 'business_form.html'
+
+    def get_success_url(self):
+        return reverse('core:index')
+
+
 index_view = IndexView.as_view()
+business_add_view = AddBusinessView.as_view()
