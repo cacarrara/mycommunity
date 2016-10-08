56196f1f581165c0031e55d66516266a46f08972
Caio Carrara <caiocarrara@gmail.com>
2016-10-08 09:08:37 -0300

    Puts core templates inside core folder

--- my_community/core/views.py
+++ my_community/core/views.py
@@ -6,18 +6,17 @@ from core.models import Business
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'core/index.html'
+
+index_view = IndexView.as_view()
 
 
 class BusinessAddView(CreateView):
     model = Business
-    fields = ['name', 'address', 'email', 'facebook_page', 'twitter_profile', 'linkedin_profile', 'business_type',
-              'segment']
-    template_name = 'business_form.html'
+    fields = ['name', 'address', 'email', 'facebook_page', 'twitter_profile',
+              'linkedin_profile', 'business_type', 'segment']
 
     def get_success_url(self):
         return reverse('core:index')
 
-
-index_view = IndexView.as_view()
 business_add_view = BusinessAddView.as_view()
